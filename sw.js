648b79c5e0e290dbcdee04411dccb69737ee8b86
Julian <91396889+Fortilian@users.noreply.github.com>
2025-10-11 16:42:48 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+// Simple app-shell cache for offline use
+const CACHE = 'poker-tikkies-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      try {
+        const copy = res.clone();
+        if (req.method === 'GET' && new URL(req.url).origin === location.origin) {
+          caches.open(CACHE).then(cache => cache.put(req, copy));
+        }
+      } catch (e) {}
+      return res;
+    }).catch(() => cached))
+  );
+});

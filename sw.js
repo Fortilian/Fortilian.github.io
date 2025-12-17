8a527f489261de034aac63c387ef6213c0876feb
Julian <91396889+Fortilian@users.noreply.github.com>
2025-12-18 00:02:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'poker-split-v2';
+const CACHE_NAME = 'poker-split-v3';
 const ASSETS = [
     './',
     './index.html',
@@ -31,3 +31,4 @@ self.addEventListener('activate', (e) => {
         })
     );
 });
+
